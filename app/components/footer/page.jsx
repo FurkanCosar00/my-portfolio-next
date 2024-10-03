933bc7b9d7ba3364392f81df7b4f4cf438a3a5c7
Furkan <furkancosar2005@gmail.com>
2024-10-03 10:21:19 +0300

    footer added, header edited

--- app/components/footer/page.jsx
+++ app/components/footer/page.jsx
@@ -1,7 +1,21 @@
+import "/app/components/footer/footer.css"
+
 export default function Footer() {
   return (
-  <>
-    <h1>Footer</h1>
-  </>
+    <footer className="footer">
+      <ul className='footer-social-links'>
+        <li><a target="_blank" href="https://www.instagram.com/luchhass/"><img src="/images/footer/instagram.png" alt="Instagram" /></a></li>
+        <li><a target="_blank" href="http://discordapp.com/users/_luchhass"><img src="/images/footer/discord.png" alt="Discord" /></a></li>
+        <li><a target="_blank" href="https://github.com/FurkanCosar00"><img src="/images/footer/github.png" alt="GitHub" /></a></li>
+      </ul>
+
+      <ul className='footer-texts'>
+        <li className="copyright">© 2023 SinanTokmak. All rights reserved.</li>
+        <li className="privacy">Privacy Policy</li>
+        <li className="terms">Terms & Conditions</li>
+      </ul>
+
+      <p className="footer-credits">Design By <span>JohannLeon</span></p>
+    </footer>
   );
 }
--- app/components/header/page.jsx
+++ app/components/header/page.jsx
@@ -4,18 +4,12 @@ import "/app/components/header/header.css"
 export default function Header() {
   return (
     <header className="header">
-      <h1><span> &lt;C/&gt; </span>FurkanCosar</h1>
+      <h1 className="header-logo"><span> &lt;C/&gt; </span>FurkanCosar</h1>
 
       <ul className="navigation">
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/projects">Projects</Link></li>
+        <li className="nav-item"><Link href="/">Home</Link></li>
+        <li className="nav-item"><Link href="/projects">Projects</Link></li>
       </ul>
-
-      {/* <ul className='headerSocials'>
-        <li><a target="_blank" href="https://www.instagram.com/luchhass/"><img src="./assets/images/header/instagramHeader.png" alt="" /></a></li>
-        <li><a target="_blank" href="http://discordapp.com/users/_luchhass"><img src="./assets/images/header/discordHeader.png" alt="" /></a></li>
-        <li><a target="_blank" href="https://github.com/FurkanCosar00"><img src="./assets/images/header/githubHeader.png" alt="" /></a></li>
-      </ul> */}
     </header>
   );
 }
