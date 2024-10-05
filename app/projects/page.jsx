a8f040d6f8ef7fd95ff23854503c1fa0a866b17b
Furkan <furkancosar2005@gmail.com>
2024-10-05 04:21:53 +0300

    projects added, footer and works fixed

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,8 +1,24 @@
+import Image from "next/image";
+import myprojects from "/app/data/myprojects.json"
+import "/app/projects/projects.css";
+
 export default function Projects() {
-    return (
-    <>
-      <h1>Projects</h1>
-    </>
-    );
-  }
+  return (
+    <div className="my-projects">
+
+      <div className="projects">
+        {/* 
+        {myprojects.reverse().map(x =>
+          <div className="projects-item">
+            <Image className="thumbnail" src={x.thumbnail} width={342} height={200}></Image>
+            <p className="title">{x.title}</p>
+            <div className="overlay"></div>
+            <div className="button"><a href="#"> siteye git </a></div>
+          </div>
+        )}
+        */}
+      </div>
+    </div>
+  )
+}
   
