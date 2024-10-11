8e9f08f7b82b4f4b42294aaa5e5b8431f79b78db
Furkan <furkancosar2005@gmail.com>
2024-10-11 14:22:00 +0300

    some projects added to data

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,24 +1,22 @@
 import Image from "next/image";
 import myprojects from "/app/data/myprojects.json"
 import "/app/projects/projects.css";
+import Link from "next/link";
 
 export default function Projects() {
   return (
     <div className="my-projects">
 
       <div className="projects">
-        {/* 
         {myprojects.reverse().map(x =>
           <div className="projects-item">
             <Image className="thumbnail" src={x.thumbnail} width={342} height={200}></Image>
             <p className="title">{x.title}</p>
             <div className="overlay"></div>
-            <div className="button"><a href="#"> siteye git </a></div>
+            <div className="button"><Link target="_blank" href={x.link}>siteye git</Link></div>
           </div>
         )}
-        */}
       </div>
     </div>
   )
 }
-  
